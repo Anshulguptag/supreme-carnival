2826f72670bf28a04cfa0e03389b3695b520ccbe
Anshulguptag <anshulguptamasteramazon@gmail.com>
2017-12-19 13:02:00 +0530

    House pricing linear Regression

---
+++ housepricinglinearregression.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+length=np.array([100,150,120,300,230,170])
+breadth=np.array([200,190,100,160,210,160])
+
+area=np.empty(6)
+
+area=length*breadth
+
+print area
